9e43ef71c8399e31c1eed8b0e3cc93a581f6d177
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-07-30 16:30:29 +0100

    Update command

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source venv/bin/activate
-exec gunicorn -b :8300 --access-logfile - --error-logfile - converter:app --timeout 3000
+exec gunicorn --chdir /home/cisconverter/converter.py -b :8300 --access-logfile - --error-logfile - converter:app --timeout 3000
