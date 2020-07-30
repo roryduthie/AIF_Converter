d0659f1f10645df8825fe48ead51d9ba662f2277
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-07-30 16:06:27 +0100

    update app name

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source venv/bin/activate
-exec gunicorn -b :8300 --access-logfile - --error-logfile - app --timeout 3000
+exec gunicorn -b :8300 --access-logfile - --error-logfile - cisconverter --timeout 3000
